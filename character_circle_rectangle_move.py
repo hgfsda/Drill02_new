9ed9d9740b2ebc5e0b3cdaf94143ea79b081fe9a
Jeong tae yeon <jtyk119@naver.com>
2023-09-21 10:23:22 +0900

    오른쪽으로 이동 가능

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -10,7 +10,6 @@ def run_circle():
     print('CIRCLE')
     
     # 일단 그림을 그리자
-
     cx, cy, r = 400, 300, 200
     
     for deg in range(0, 360, 1):
@@ -20,14 +19,19 @@ def run_circle():
         grass.draw_now(400,30)
         character.draw_now(x,y)
         delay(0.01)
-    pass
 
 def run_rectangle():
     print('RECTANGLE')
+
+    for x in range(50,750 + 1,10):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,90)
+        delay(0.01)
     pass
 
 while True:
-    run_circle()
+    #run_circle()
     run_rectangle()
     break
 
